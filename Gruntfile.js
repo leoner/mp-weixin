7cb6b13e99e2ead172080e7855d7031c83bb9419
kangpangpang <kangpangpang@gmail.com>
2013-05-08 16:32:39 +0800

    update content

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,8 @@ module.exports = function(grunt) {
     jshint: {
       all: [
           'Gruntfile.js',
-          'lib/**/*.js'
+          'lib/**/*.js',
+          'messages/*.json'
       ],
       options: {
           'jshintrc': '.jshintrc'
